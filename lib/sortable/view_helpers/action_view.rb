60020521fe94774d4d3e37f3f3b5475f294e9909
Scott Pullen <s.pullen05@gmail.com>
2012-12-27 15:30:53 -0500

    testing function visibility

--- lib/sortable/view_helpers/action_view.rb
+++ lib/sortable/view_helpers/action_view.rb
@@ -8,7 +8,7 @@ module Sortable
         title ||= column.titleize
         css_class = (column == sort_column) ? "sortable-current sortable-#{sort_direction}" : nil
         direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-        link_to title, {:controller => controller_name, :action => action_name, :sort => column, :direction => direction}, {:class => css_class}.merge(html_options)
+        link_to title, params.merge(:sort => column, :direction => direction), {:class => css_class}.merge(html_options)
       end
       
     end
--- spec/action_controller_spec.rb
+++ spec/action_controller_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
-describe Sortable::ActionController do
+class TestController < AbstractController::Base
+  include Sortable::ActionController
+end
 
+# get access to the protected methods
+TestController.send(:public, *TestController.protected_instance_methods)
+
+describe Sortable::ActionController do
+  
+  it 'should respond to sortable' do
+    TestController.should respond_to(:sortable)
+  end
+  
+  describe 'instance methods' do
+  
+    before(:each) do
+      @controller = TestController.new
+    end
+    
+    it 'should respond to sort_column' do
+      @controller.should respond_to(:sort_column)
+    end
+    
+    it 'should respond to sort_direction' do
+      @controller.should respond_to(:sort_direction)
+    end
+  
+  end
 end
