e837d7b6367c097c311eade678091768c30e3881
Scott Pullen <s.pullen05@gmail.com>
2012-12-27 15:01:03 -0500

    updating the active record integrations

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe Sortable::ActiveRecord do
   
   it 'should have a sortable class method' do
-    Quote.methods.include?(:sortable).should be_true
+    Quote.should respond_to(:sortable)
   end
   
   it 'should have a sort class method' do
-    Quote.methods.include?(:sort).should be_true
+    Quote.should respond_to(:sort)
   end
   
   describe '.sortable' do
